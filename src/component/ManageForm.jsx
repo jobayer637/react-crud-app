36d0c66ea7cd6bdff2d42cba76651a3ae0d6bbe7
Jobayer Hossain <web.jobayer@gmail.com>
2021-09-20 19:39:21 +0600

    update manage input

--- src/component/ManageForm.jsx
+++ src/component/ManageForm.jsx
@@ -2,52 +2,41 @@ import React from 'react'
 import {
     Button, Form, Row, Col
 } from 'react-bootstrap'
+import ManageInput from './ManageInput'
 
-const ManageForm = ({newEmp, handleInput, handleForm}) => {
+const ManageForm = ({ newEmp, handleInput, handleForm }) => {
     return (
         <div>
             <Form onSubmit={handleForm}>
                 <Row className="align-items-center">
                     <Col md="4">
-                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-                            Name
-                        </Form.Label>
-                        <Form.Control
-                            onChange={handleInput}
-                            type="text"
-                            name="name"
+                        <ManageInput
+                            label={'Name'}
+                            handleInput={handleInput}
+                            type={'text'}
+                            name={'name'}
                             value={newEmp.name}
-                            className="mb-2"
-                            id="inlineFormInput"
-                            placeholder="Enter Name"
+                            placeholder={'Enter Employee Name'}
                         />
                     </Col>
                     <Col md="3">
-                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-                            Name
-                        </Form.Label>
-                        <Form.Control
-                            onChange={handleInput}
-                            type="text"
-                            name="salary"
+                        <ManageInput
+                            label={'Salary'}
+                            handleInput={handleInput}
+                            type={'text'}
+                            name={'salary'}
                             value={newEmp.salary}
-                            className="mb-2"
-                            id="inlineFormInput"
-                            placeholder="Enter Salary"
+                            placeholder={'Enter Employee Salary'}
                         />
                     </Col>
                     <Col xs="auto">
-                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-                            Name
-                        </Form.Label>
-                        <Form.Control
-                            onChange={handleInput}
-                            type="text"
-                            name="age"
+                        <ManageInput
+                            label={'Age'}
+                            handleInput={handleInput}
+                            type={'text'}
+                            name={'age'}
                             value={newEmp.age}
-                            className="mb-2"
-                            id="inlineFormInput"
-                            placeholder="Enter Age"
+                            placeholder={'Enter Employee Age'}
                         />
                     </Col>
                     <Col xs="auto">
--- src/component/ManageInput.jsx
+++ src/component/ManageInput.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {
+    Form
+} from 'react-bootstrap'
 
-const ManageInput = () => {
+
+const ManageInput = ({label, handleInput, type, name, value, placeholder}) => {
     return (
         <div>
-            
+            <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                {label}
+            </Form.Label>
+            <Form.Control
+                onChange={handleInput}
+                type={type}
+                name={name}
+                value={value}
+                className="mb-2"
+                id="inlineFormInput"
+                placeholder={placeholder}
+            />
         </div>
     )
 }
